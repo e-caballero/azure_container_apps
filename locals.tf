e610040dffc4bc20ed77834722047a3f257a9f20
erik caballero <erik.caballero@icloudnetwork.net>
2025-02-21 09:43:15 -0500

    fixed syntax issue with trim name

--- locals.tf
+++ locals.tf
@@ -5,24 +5,26 @@ locals {
   # - end with alphanumeric
   # - no double hyphens
   # - max 32 chars
-  formatted_name = function(name) {
-    lower(
-      substr(
-        replace(
+  formatted_name = (
+    function(name) {
+      lower(
+        substr(
           replace(
             replace(
-              name,
-              "--", "-"
+              replace(
+                name,
+                "--", "-"
+              ),
+              "/[^a-zA-Z0-9-]/", ""
             ),
-            "/[^a-zA-Z0-9-]/", ""
+            "/^[^a-zA-Z]+/", ""
           ),
-          "/^[^a-zA-Z]+/", ""
-        ),
-        0,
-        32
+          0,
+          32
+        )
       )
-    )
-  }
+    }
+  )
 
   formatted_rg_name = local.formatted_name(var.resource_group_name)
   formatted_container_app_name = local.formatted_name(var.container_app_name)
