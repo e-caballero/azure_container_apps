62f871f394f6074777a93da9e02f638b4bbc5e53
erik caballero <erik.caballero@icloudnetwork.net>
2025-04-21 09:36:58 -0400

    workload profile support added

--- container_apps.tf
+++ container_apps.tf
@@ -22,6 +22,7 @@ resource "azurerm_container_app_environment" "container_app_env" {
   resource_group_name        = azurerm_resource_group.resource_group.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
   tags                       = var.common_tags
+  workload_profile_type      = "Consumption"
   
   workload_profile {
     name          = "Consumption"
